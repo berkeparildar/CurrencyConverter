44278f4f55062857ca81d7f700c6c70bbc0a53a3
Berke Parıldar <94047726+berkeparildar@users.noreply.github.com>
2024-01-16 02:27:20 +0300

    Initial commit

---
+++ CurrencyConverter/CurrencyConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConverterApp.swift
+//  CurrencyConverter
+//
+//  Created by Berke Parıldar on 16.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
